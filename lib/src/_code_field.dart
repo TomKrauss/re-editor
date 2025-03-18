4a64f9cdc90f35f3b6442ce40ec97d8474f2217a
Frogperson <frgprsn@gmail.com>
2025-03-18 14:59:06 -0400

    Allow custom span cursors

--- lib/src/_code_field.dart
+++ lib/src/_code_field.dart
@@ -848,6 +848,10 @@ class _CodeFieldRender extends RenderBox implements MouseTrackerAnnotation {
       result.add(BoxHitTestEntry(this, position));
       final CodeLineRenderParagraph? paragraph = _findDisplayRenderParagraph(position + paintOffset);
       final InlineSpan? span = paragraph?.getSpanForPosition(position - paragraph.offset + paintOffset);
+      MouseCursor? spanCursor;
+      if (span is TextSpan && span.cursor != MouseCursor.defer) {
+        spanCursor = span.cursor;
+      }
       if (span is MouseTrackerAnnotationTextSpan) {
         result.add(HitTestEntry(_MouseTrackerAnnotationTextSpan(
           id: paragraph!.index,
@@ -862,7 +866,7 @@ class _CodeFieldRender extends RenderBox implements MouseTrackerAnnotation {
       if (_chunkIndicators.where((chunk) => chunk.canExpand && chunk.region.contains(position)).isNotEmpty) {
         _cursor = SystemMouseCursors.click;
       } else {
-        _cursor = SystemMouseCursors.text;
+        _cursor = spanCursor ?? SystemMouseCursors.text;
       }
       hitTarget = true;
     }
